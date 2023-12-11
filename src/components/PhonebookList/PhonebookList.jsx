3c104ce21be546863c71a71fb14f95ee8040ad20
Br0kEnM1Nd-Git <mrgamer594@gmail.com>
2023-12-11 21:19:06 +0200

    add persistor lib usage

--- src/components/PhonebookList/PhonebookList.jsx
+++ src/components/PhonebookList/PhonebookList.jsx
@@ -1,25 +1,12 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { ContactsList } from './PhonebookList.styled';
-import { useEffect } from 'react';
 import { getContactsSelector, getFilterSelector } from 'store/selectors';
-import { deleteContactAction, setContactsAction } from 'store/contactsSlice';
+import { deleteContactAction } from 'store/contactsSlice';
 
 export const PhonebookList = () => {
   const contacts = useSelector(getContactsSelector);
   const filter = useSelector(getFilterSelector);
   const dispatch = useDispatch();
-
-  useEffect(() => {
-    const localData = JSON.parse(localStorage.getItem('contacts'));
-    localData && dispatch(setContactsAction(localData));
-  }, [dispatch]);
-
-  useEffect(() => {
-    setTimeout(
-      () => localStorage.setItem('contacts', JSON.stringify(contacts)),
-      200
-    );
-  }, [contacts]);
 
   const deleteContact = id => {
     dispatch(deleteContactAction(id));
--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import App from 'App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from 'store/store';
+import store, { persistor } from 'store/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter basename="goit-react-hw-06-phonebook">
     <React.StrictMode>
       <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </React.StrictMode>
   </BrowserRouter>
